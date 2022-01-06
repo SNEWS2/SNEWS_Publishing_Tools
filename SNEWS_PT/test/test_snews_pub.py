c834fe457c4b6a88aa34b59f29621225d4d24006
joesmolsky <joesmolsky@gmail.com>
2022-01-05 19:47:43 -0500

    add basic CoincidenceTier unit test

---
+++ SNEWS_PT/test/test_snews_pub.py
@@ -0,0 +1,16 @@
+from SNEWS_PT.snews_pub import Publisher
+from SNEWS_PT.snews_pub import CoincidenceTier
+
+def test_send_CoincidenceTier_typical():
+    """Test sending a typical coincidence tier message."""
+    with Publisher() as pub:
+        message = CoincidenceTier(detector_name='KamLAND', neutrino_time="01:23:45:678901", p_value = 0.01).message()
+        pub.send(message)
+    pass
+
+def test_CoincidenceTier_message_basic():
+    """Test creating a message with an invalid detector."""
+    message = CoincidenceTier(detector_name='KamLAND', neutrino_time="01:23:45:678901", p_value = 0.01).message()
+    assert message['detector_name'] == 'KamLAND'
+    assert message['neutrino_time'] == '01:23:45:678901'
+    assert message['p_value'] == 0.01
