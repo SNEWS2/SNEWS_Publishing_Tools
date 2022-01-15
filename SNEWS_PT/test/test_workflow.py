000146c5f6f9f52e39e424339dcfb9eedee8b42c
joesmolsky <joesmolsky@gmail.com>
2022-01-15 09:27:40 -0500

    workflow pub test

--- SNEWS_PT/test/test_workflow.py
+++ SNEWS_PT/test/test_workflow.py
@@ -6,3 +6,15 @@ def test_CoincidenceTier_message_keys():
     assert message['detector_name'] == 'TEST'
     assert message['neutrino_time'] == '01:23:45:678901'
     assert message['p_value'] == 0.01
+
+def test_send_CoincidenceTier_working():
+    """Test sending a coincidence tier message.
+    
+    Note
+    ----
+    Checks if SNEWS_PT can import and run.  Does not check for accuracy.
+    """
+    with Publisher() as pub:
+        message = CoincidenceTier(detector_name='TEST', neutrino_time="01:23:45:678901", p_value = 0.01).message()
+        pub.send(message)
+    pass
