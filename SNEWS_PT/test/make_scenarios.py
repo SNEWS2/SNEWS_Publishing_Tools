c9a0f50d1549e2973e7b63aa1966babdd631559e
Melih <melih.kara@kit.edu>
2022-02-06 12:58:45 +0100

    added Ricardo's scenario added a CLI tool for scenario tests

--- SNEWS_PT/test/make_scenarios.py
+++ SNEWS_PT/test/make_scenarios.py
@@ -24,13 +24,43 @@ msg6 = {"detector_name": "JUNO",
         "neutrino_time": "22/01/01 12:30:56:678999",
         "p_value": 0.98}
 
+####
+msg7 = {"detector_name": "Borexino",
+        "neutrino_time": "22/01/01 12:30:10:678999",
+        "p_value": 0.98}
+
+msg8 = {"detector_name": "ICE",
+        "neutrino_time": "22/01/01 12:30:19:678999",
+        "p_value": 0.98}
+
+msg9 = {"detector_name": "NOvA",
+        "neutrino_time": "22/01/01 12:30:21:678999",
+        "p_value": 0.98}
+
+msg10 = {"detector_name": "DS-20K",
+        "neutrino_time": "22/01/01 12:30:22:678999",
+        "p_value": 0.98}
+
+msg11 = {"detector_name": "HALO",
+        "neutrino_time": "22/01/01 12:30:20:678999",
+        "p_value": 0.98}
+
+msg12 = {"detector_name": "XENONnT",
+        "neutrino_time": "22/01/01 12:30:29:678999",
+        "p_value": 0.98}
+
+msg13 = {"detector_name": "JUNO",
+        "neutrino_time": "22/01/01 12:30:18:678999",
+        "p_value": 0.98}
+
 scenarios = {
     "simple coincidence": [msg1,msg2],
     "exact 10s coincidence": [msg1, msg3],
     "two coincidences":[msg1,msg2, msg4, msg5],
-    "same detector submits twice":[msg1, msg1],
-    "same detector submits sequentially":[msg4, msg6],
-    "times are out of order":[msg3, msg2, msg1]
+    "same message submitted twice":[msg1, msg1],
+    "same detector submits within 1sec":[msg4, msg6],
+    "3 coincident message, out of order":[msg3, msg2, msg1],
+    "msgs at 10, 19, 21, 22, 20, 29, 18 seconds": [msg7,msg8,msg9,msg10,msg11,msg12,msg13],
 }
 
 with open("scenarios.json", "w") as write_file:
