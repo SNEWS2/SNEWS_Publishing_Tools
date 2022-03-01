65752c0e9f62d186124fca748ce13c159e05fa1e
joesmolsky <joesmolsky@gmail.com>
2022-03-01 08:30:22 -0500

    update testing keys

--- SNEWS_PT/test/test_timing_tier.py
+++ SNEWS_PT/test/test_timing_tier.py
@@ -8,8 +8,8 @@ def test_timing_expected():
 
     # Check that message has expected structure.
     assert tims.tiernames == ['CoincidenceTier', 'TimeTier']
-    assert tims.message_data == {'detector_name': 'XENONnT', 'machine_time': None, 'neutrino_time': '12/06/09 15:31:08:1098', 'p_val': None, 'p_values': None, 'timing_series': ['12/06/09 15:31:08:1098', '12/06/09 15:33:07:8910'], 'which_tier': None, 'n_retract_latest': None, 'retraction_reason': None, 'detector_status': None, 'is_pre_sn': False}
-    assert tims.messages == [{'_id': '19_CoincidenceTier_None', 'detector_name': 'XENONnT', 'machine_time': None, 'neutrino_time': '12/06/09 15:31:08:1098', 'p_val': None, 'meta': {}, 'schema_version': '1.1.0'}, {'_id': '19_TimeTier_None', 'detector_name': 'XENONnT', 'machine_time': None, 'neutrino_time': '12/06/09 15:31:08:1098', 'timing_series': ['12/06/09 15:31:08:1098', '12/06/09 15:33:07:8910'], 'meta': {}, 'schema_version': '1.1.0'}]
+    assert tims.message_data == {'detector_name': 'XENONnT', 'machine_time': None, 'neutrino_time': '12/06/09 15:31:08:1098', 'p_val': None, 'p_values': None, 'timing_series': ['12/06/09 15:31:08:1098', '12/06/09 15:33:07:8910'], 'which_tier': None, 'n_retract_latest': None, 'retraction_reason': None, 'detector_status': None, 'is_pre_sn': False, 'meta': {}}
+    assert tims.messages == [{'_id': '19_CoincidenceTier_None', 'detector_name': 'XENONnT', 'machine_time': None, 'neutrino_time': '12/06/09 15:31:08:1098', 'p_val': None, 'meta': {'meta': {}}, 'schema_version': '1.1.0'}, {'_id': '19_TimeTier_None', 'detector_name': 'XENONnT', 'machine_time': None, 'neutrino_time': '12/06/09 15:31:08:1098', 'timing_series': ['12/06/09 15:31:08:1098', '12/06/09 15:33:07:8910'], 'meta': {'meta': {}}, 'schema_version': '1.1.0'}]
     assert tims.env_file == None
     # Try to send message to SNEWS 2.0 server.
     try:
