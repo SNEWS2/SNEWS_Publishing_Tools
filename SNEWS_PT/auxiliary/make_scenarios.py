181ed533dbc5768b203f88d609c5954e68a6250e
Melih Kara <kara@kit.edu>
2022-03-02 18:41:33 +0100

    all test messages are in the future now

--- SNEWS_PT/auxiliary/make_scenarios.py
+++ SNEWS_PT/auxiliary/make_scenarios.py
@@ -1,85 +1,85 @@
 import json
 
 msg1 = {"detector_name": "XENONnT",
-        "neutrino_time": "22/01/01 12:34:45:678999",
+        "neutrino_time": "30/01/01 12:34:45:678999",
         "p_val": 0.98}
 
 msg2 = {"detector_name": "DS-20K",
-        "neutrino_time": "22/01/01 12:34:47:678999",
+        "neutrino_time": "30/01/01 12:34:47:678999",
         "p_val": 0.98}
 
 msg3 = {"detector_name": "DUNE",
-        "neutrino_time": "22/01/01 12:34:55:678999",
+        "neutrino_time": "30/01/01 12:34:55:678999",
         "p_val": 0.98}
 
 msg4 = {"detector_name": "JUNO",
-        "neutrino_time": "22/01/01 12:30:55:678999",
+        "neutrino_time": "30/01/01 12:30:55:678999",
         "p_val": 0.98}
 
 msg5 = {"detector_name": "Baksan",
-        "neutrino_time": "22/01/01 12:30:52:678999",
+        "neutrino_time": "30/01/01 12:30:52:678999",
         "p_val": 0.98}
 
 msg6 = {"detector_name": "JUNO",
-        "neutrino_time": "22/01/01 12:30:56:678999",
+        "neutrino_time": "30/01/01 12:30:56:678999",
         "p_val": 0.98}
 
 ####
 msg7 = {"detector_name": "Borexino",
-        "neutrino_time": "22/01/01 12:30:10:678999",
+        "neutrino_time": "30/01/01 12:30:10:678999",
         "p_val": 0.98}
 
 msg8 = {"detector_name": "ICE",
-        "neutrino_time": "22/01/01 12:30:19:678999",
+        "neutrino_time": "30/01/01 12:30:19:678999",
         "p_val": 0.98}
 
 msg9 = {"detector_name": "NOvA",
-        "neutrino_time": "22/01/01 12:30:21:678999",
+        "neutrino_time": "30/01/01 12:30:21:678999",
         "p_val": 0.98}
 
 msg10 = {"detector_name": "DS-20K",
-        "neutrino_time": "22/01/01 12:30:22:678999",
+        "neutrino_time": "30/01/01 12:30:22:678999",
         "p_val": 0.98}
 
 msg11 = {"detector_name": "HALO",
-        "neutrino_time": "22/01/01 12:30:20:678999",
+        "neutrino_time": "30/01/01 12:30:20:678999",
         "p_val": 0.98}
 
 msg12 = {"detector_name": "XENONnT",
-        "neutrino_time": "22/01/01 12:30:29:678999",
+        "neutrino_time": "30/01/01 12:30:29:678999",
         "p_val": 0.98}
 
 msg13 = {"detector_name": "JUNO",
-        "neutrino_time": "22/01/01 12:30:18:678999",
+        "neutrino_time": "30/01/01 12:30:18:678999",
         "p_val": 0.98}
 
 ####
 msg7 = {"detector_name": "Borexino",
-        "neutrino_time": "22/01/01 12:30:10:678999",
+        "neutrino_time": "30/01/01 12:30:10:678999",
         "p_value": 0.98}
 
 msg8 = {"detector_name": "ICE",
-        "neutrino_time": "22/01/01 12:30:19:678999",
+        "neutrino_time": "30/01/01 12:30:19:678999",
         "p_value": 0.98}
 
 msg9 = {"detector_name": "NOvA",
-        "neutrino_time": "22/01/01 12:30:21:678999",
+        "neutrino_time": "30/01/01 12:30:21:678999",
         "p_value": 0.98}
 
 msg10 = {"detector_name": "DS-20K",
-        "neutrino_time": "22/01/01 12:30:22:678999",
+        "neutrino_time": "30/01/01 12:30:22:678999",
         "p_value": 0.98}
 
 msg11 = {"detector_name": "HALO",
-        "neutrino_time": "22/01/01 12:30:20:678999",
+        "neutrino_time": "30/01/01 12:30:20:678999",
         "p_value": 0.98}
 
 msg12 = {"detector_name": "XENONnT",
-        "neutrino_time": "22/01/01 12:30:29:678999",
+        "neutrino_time": "30/01/01 12:30:29:678999",
         "p_value": 0.98}
 
 msg13 = {"detector_name": "JUNO",
-        "neutrino_time": "22/01/01 12:30:18:678999",
+        "neutrino_time": "30/01/01 12:30:18:678999",
         "p_value": 0.98}
 
 scenarios = {
