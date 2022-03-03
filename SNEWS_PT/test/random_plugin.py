4eabfa87cc1fe99829fb50651df6ff738e2e531b
Melih Kara <kara@kit.edu>
2022-03-03 17:59:49 +0100

    fix argv

--- SNEWS_PT/test/random_plugin.py
+++ SNEWS_PT/test/random_plugin.py
@@ -4,8 +4,9 @@ import json
 
 # It takes the alert message as a first input
 print(f"Received alert")
-alert_message = json.loads(argv[1])
+alert_message = json.loads(sys.argv[1])
 print(f"Parsed as a dictionary")
+print(f"Running custom plugin {sys.argv[0]}")
 print(f"There are these keys; {alert_message.keys()}")
 print(f"The message content is \n {alert_message}")
 print(f"Do fancy stuff here with the freshly baked alert message")
