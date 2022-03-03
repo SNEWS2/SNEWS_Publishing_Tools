b6f42bfaddf9e04d0da5346c1a1c47c107f900c9
Melih Kara <kara@kit.edu>
2022-03-03 17:57:13 +0100

    random plugin example

---
+++ SNEWS_PT/test/random_plugin.py
@@ -0,0 +1,11 @@
+""" This is a random plugin to test `subscribe_and_redirect_alert """
+import sys
+import json
+
+# It takes the alert message as a first input
+print(f"Received alert")
+alert_message = json.loads(argv[1])
+print(f"Parsed as a dictionary")
+print(f"There are these keys; {alert_message.keys()}")
+print(f"The message content is \n {alert_message}")
+print(f"Do fancy stuff here with the freshly baked alert message")
