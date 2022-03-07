7fde350c8f85ffa78ae5bd29b6ddf5f32d5d829e
Melih Kara <kara@kit.edu>
2022-03-07 11:00:00 +0100

    fix tests

--- SNEWS_PT/test/test_coincidence_tier.py
+++ SNEWS_PT/test/test_coincidence_tier.py
@@ -11,7 +11,7 @@ def test_coincidence_expected():
                                  'p_val': None, 'p_values': None, 'timing_series': None, 'which_tier': None, 'n_retract_latest': None, 
                                  'retraction_reason': None, 'detector_status': None, 'is_pre_sn': False, 't_bin_width': None, 'meta': {'p_value': 0.4}}
     assert coin.messages == [{'_id': '4_CoincidenceTier_None', 'detector_name': 'KamLAND', 'machine_time': None, 'neutrino_time': '12/06/09 15:31:08:1098', 
-                              't_bin_width': None, 'p_val': None, 'meta': {'meta': {'p_value': 0.4}}, 'schema_version': '1.1.0'}]
+                              'p_val': None, 'meta': {'meta': {'p_value': 0.4}}, 'schema_version': '1.1.0'}]
     assert coin.env_file == None
     # Try to send message to SNEWS 2.0 server.
     try:
