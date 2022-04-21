58374579a0b1aa533b5ce2ce72291226f4256996
Melih Kara <kara@kit.edu>
2022-04-21 11:01:01 +0200

    update tests, don't check _id

--- SNEWS_PT/test/test_coincidence_tier.py
+++ SNEWS_PT/test/test_coincidence_tier.py
@@ -10,8 +10,12 @@ def test_coincidence_expected():
     assert coin.message_data == {'detector_name': 'KamLAND', 'machine_time': None, 'neutrino_time': '12/06/09 15:31:08:1098', 
                                  'p_val': None, 'p_values': None, 'timing_series': None, 'which_tier': None, 'n_retract_latest': None, 
                                  'retraction_reason': None, 'detector_status': None, 'is_pre_sn': False, 't_bin_width': None, 'meta': {'p_value': 0.4}}
-    assert coin.messages == [{'_id': '4_CoincidenceTier_None', 'detector_name': 'KamLAND', 'machine_time': None, 'neutrino_time': '12/06/09 15:31:08:1098', 
-                              'p_val': None, 'meta': {'meta': {'p_value': 0.4}}, 'schema_version': '1.1.0'}]
+
+    input_messages = {'detector_name': 'KamLAND', 'machine_time': None, 'neutrino_time': '12/06/09 15:31:08:1098',
+                      'p_val': None, 'meta': {'meta': {'p_value': 0.4}}, 'schema_version': '1.1.0'}
+    for k,v in input_messages.items():
+        assert coin.messages[0][k] == v
+
     assert coin.env_file == None
     # Try to send message to SNEWS 2.0 server.
     try:
