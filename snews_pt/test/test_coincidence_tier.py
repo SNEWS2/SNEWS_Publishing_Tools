fae273556900612857e93f38ecba556555b879da
Melih Kara <kara@kit.edu>
2022-07-28 16:32:59 +0200

    fix tests

--- snews_pt/test/test_coincidence_tier.py
+++ snews_pt/test/test_coincidence_tier.py
@@ -1,21 +1,24 @@
 """Test publishing coincidence tier messages."""
 from snews_pt.snews_pub import SNEWSTiersPublisher
+from snews_pt.snews_pt_utils import set_name
 
 def test_coincidence_expected():
     """Test with example of expected message type."""
     # Create coincidence tier message.
+    set_name(detector_name="KamLAND")
     coin = SNEWSTiersPublisher(detector_name='KamLAND', neutrino_time='12/06/09 15:31:08:1098', p_value=0.4,
                                firedrill_mode=False)
     # Check that message has expected structure.
     assert coin.tiernames == ['CoincidenceTier']
     assert coin.message_data == {'detector_name': 'KamLAND', 'machine_time': None, 'neutrino_time': '12/06/09 15:31:08:1098', 
                                  'p_val': None, 'p_values': None, 'timing_series': None, 'which_tier': None, 'n_retract_latest': None, 
-                                 'retraction_reason': None, 'detector_status': None, 'is_pre_sn': False, 't_bin_width': None, 'meta': {'p_value': 0.4}}
+                                 'retraction_reason': None, 'detector_status': None, 'is_pre_sn': False, 't_bin_width': None,
+                                 'p_value': 0.4}
 
     input_messages = {'detector_name': 'KamLAND', 'machine_time': None, 'neutrino_time': '12/06/09 15:31:08:1098',
-                      'p_val': None, 'meta': {'meta': {'p_value': 0.4}}, 'schema_version': '1.1.0'}
+                      'p_val': None, 'meta': {'p_value': 0.4}, 'schema_version': '1.1.0'}
     for k,v in input_messages.items():
-        if k == 'sent_time':
+        if k in ['sent_time', 'machine_time']:
             continue
         assert coin.messages[0][k] == v
 
--- snews_pt/test/test_timing_tier.py
+++ snews_pt/test/test_timing_tier.py
@@ -1,24 +1,29 @@
 """Test publishing timing tier messages."""
 from snews_pt.snews_pub import SNEWSTiersPublisher
+from snews_pt.snews_pt_utils import set_name
 
 def test_timing_expected():
     """Test with example of expected message type."""
     # Create timing tier message.
+    set_name(detector_name="XENONnT")
     tims = SNEWSTiersPublisher(detector_name='XENONnT', neutrino_time='12/06/09 15:31:08:1098', timing_series=['12/06/09 15:31:08:1098', '12/06/09 15:33:07:8910'],
                                firedrill_mode=False)
 
     # Check that message has expected structure.
     assert tims.tiernames == ['CoincidenceTier', 'TimeTier']
-    assert tims.message_data == {'detector_name': 'XENONnT', 'machine_time': None, 'neutrino_time': '12/06/09 15:31:08:1098', 'p_val': None, 'p_values': None, 
-                                 'timing_series': ['12/06/09 15:31:08:1098', '12/06/09 15:33:07:8910'], 'which_tier': None, 'n_retract_latest': None, 
-                                 'retraction_reason': None, 'detector_status': None, 'is_pre_sn': False, 't_bin_width': None, 'meta': {}}
+    assert tims.message_data == {'detector_name': 'XENONnT', 'machine_time': None, 'neutrino_time': '12/06/09 15:31:08:1098', 'p_val': None, 'p_values': None,
+                                 'timing_series': ['12/06/09 15:31:08:1098', '12/06/09 15:33:07:8910'], 'which_tier': None, 'n_retract_latest': None,
+                                 'retraction_reason': None, 'detector_status': None, 'is_pre_sn': False, 't_bin_width': None, }
 
     input_messages = [{'detector_name': 'XENONnT', 'machine_time': None, 'neutrino_time': '12/06/09 15:31:08:1098',
-                     'p_val': None, 'meta': {'meta': {}}, 'schema_version': '1.1.0'}, {'detector_name': 'XENONnT', 'machine_time': None,
-                     'neutrino_time': '12/06/09 15:31:08:1098', 'timing_series': ['12/06/09 15:31:08:1098', '12/06/09 15:33:07:8910'], 'meta': {'meta': {}}, 'schema_version': '1.1.0'}]
+                     'p_val': None, 'meta': {}, 'schema_version': '1.1.0'},
+                      {'detector_name': 'XENONnT', 'machine_time': None,
+                     'neutrino_time': '12/06/09 15:31:08:1098',
+                       'timing_series': ['12/06/09 15:31:08:1098', '12/06/09 15:33:07:8910'],
+                       'meta': {}, 'schema_version': '1.1.0'}]
     for i, message in enumerate(input_messages):
         for k,v in message.items():
-            if k == 'sent_time':
+            if k in ['sent_time', 'machine_time']:
                 continue
             assert tims.messages[i][k] == v
 
