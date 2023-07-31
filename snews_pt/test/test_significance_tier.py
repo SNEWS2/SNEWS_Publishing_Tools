22c03488f6ec467f5538112aa9ad5c0654e04c81
Melih Kara <kara@kit.edu>
2023-07-31 10:19:51 +0200

    fix significance unit test

--- snews_pt/test/test_significance_tier.py
+++ snews_pt/test/test_significance_tier.py
@@ -1,13 +1,13 @@
-"""Test publishing significane tier messages."""
-from snews_pt.snews_pub import SNEWSTiersPublisher
-from snews_pt.snews_format_checker import SnewsFormat
+"""Test publishing significant tier messages."""
+from snews_pt.messages import SNEWSMessageBuilder
 
 def test_significance_expected():
     """Test with example of expected message type."""
     # Create significance tier message.
-    sign = SNEWSTiersPublisher(detector_name='DS-20K',
-                               neutrino_time = '2012-06-09T15:31:08.109876',
-                               neutrino_times=['2012-06-09T15:31:08.109876', 
+    sign = SNEWSMessageBuilder(detector_name='DS-20K',
+                               machine_time='2012-06-09T15:30:00.000501',
+                               neutrino_time='2012-06-09T15:31:08.109876',
+                               neutrino_times=['2012-06-09T15:31:08.109876',
                                                '2012-06-09T15:33:07.891098'],
                                p_values=[0.4, 0.5],
                                t_bin_width=0.8,
@@ -15,35 +15,40 @@ def test_significance_expected():
                                is_test=True)
 
     # Check that message has expected structure.
-    assert list(sign.tiernames) == ['CoincidenceTier', 'SigTier']
-    assert sign.message_data == {'detector_name': 'DS-20K',
-                                'machine_time': None,
-                                'neutrino_time': '2012-06-09T15:31:08.109876',
-                                'p_val': None,
-                                'p_values': [0.4, 0.5],
-                                't_bin_width': 0.8,
-                                'timing_series': None,
-                                'retract_latest': None,
-                                'retraction_reason': None,
-                                'detector_status': None,
-                                'is_pre_sn': False,
-                                'neutrino_times': ['2012-06-09T15:31:08.109876',
-                                                   '2012-06-09T15:33:07.891098'],
-                                'is_test':True}
-    assert sign.env_file is None
+    assert sign.selected_tiers == ['SNEWSCoincidenceTierMessage', 'SNEWSSignificanceTierMessage']
+    assert sign.messages[0].message_data == {'_id': 'DS-20K_CoincidenceTier_2012-06-09T15:30:00.000501',
+                                             'schema_version': '1.3.0',
+                                             'detector_name': 'DS-20K',
+                                             'machine_time': '2012-06-09T15:30:00.000501',
+                                             'neutrino_time': '2012-06-09T15:31:08.109876',
+                                             'p_val': None}
 
-    # the SNEWSTierPublisher already checks this in creation, but we can double check
-    for message in sign.messages:
-        format_checker = SnewsFormat(message)
-        assert format_checker() is True, "Message is not in the snews format"
+    assert sign.messages[0].meta == {'p_values': [0.4, 0.5],
+                                     't_bin_width': 0.8,
+                                     'is_test': True,
+                                     'neutrino_times': ['2012-06-09T15:31:08.109876',
+                                                        '2012-06-09T15:33:07.891098'],
+                                     'firedrill_mode': False}
 
-    assert len(sign.invalid_messages)==0, "There are invalid messages"
+    assert sign.messages[1].message_data == {'_id': 'DS-20K_SignificanceTier_2012-06-09T15:30:00.000501',
+                                             'schema_version': '1.3.0',
+                                             'detector_name': 'DS-20K',
+                                             'machine_time': '2012-06-09T15:30:00.000501',
+                                             'p_values': [0.4, 0.5],
+                                             't_bin_width': 0.8}
+
+    assert sign.messages[1].meta == {'neutrino_time': '2012-06-09T15:31:08.109876',
+                                     'is_test': True,
+                                     'neutrino_times': ['2012-06-09T15:31:08.109876',
+                                                        '2012-06-09T15:33:07.891098'],
+                                     'firedrill_mode': False}
+
+    assert sign.messages[0].is_valid() is True, "invalid coincidence tier message in 'test_significance_tier'"
+    assert sign.messages[1].is_valid() is True, "invalid significance tier message in 'test_significance_tier'"
 
     # Try to send message to SNEWS 2.0 server.
     try:
-        sign.send_to_snews()
+        sign.send_messages()
     except Exception as exc:
         print('SNEWSTiersPublisher.send_to_snews() test failed!\n')
         assert False, f"Exception raised:\n {exc}"
-
-test_significance_expected()
