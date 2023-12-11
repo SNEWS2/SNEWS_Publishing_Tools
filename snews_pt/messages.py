3cc9729847cf7d71646fb4fb6e2d6cb2b63183bc
KaraMelih <kara@kit.edu>
2023-12-11 16:55:24 +0100

    fix detector name set reintroduce meta field add is_test to accepted fields

--- snews_pt/messages.py
+++ snews_pt/messages.py
@@ -129,6 +129,7 @@ class SNEWSMessage(ABC):
             machine_time = raw_mt
             )
 
+        self.is_test = kwargs.get('is_test', False)
         for kw in kwargs:
             if kw in fields:
                 # Append all kwargs matching subclass fields in message_data.
@@ -137,7 +138,6 @@ class SNEWSMessage(ABC):
                 # Append all non-matching kwargs to meta.
                 self.meta[kw] = kwargs[kw]
 
-        self.is_test = kwargs.get('is_test', False)
         # Check that required fields are present and valid.
         self.has_required_fields()
 
@@ -166,6 +166,8 @@ class SNEWSMessage(ABC):
         """
         if detector_name == 'TEST' or detector_name is None:
             detector_name = snews_pt_utils.get_name()
+        else:
+            detector_name = snews_pt_utils.set_name(detector_name, _return=True)
         return detector_name
 
     def clean_time_input(self, time):
@@ -242,7 +244,7 @@ class SNEWSCoincidenceTierMessage(SNEWSMessage):
     """Message for SNEWS 2.0 coincidence tier."""
 
     reqfields = [ 'neutrino_time' ]
-    fields = SNEWSMessage.basefields + reqfields + [ 'machine_time', 'p_val' ]
+    fields = SNEWSMessage.basefields + reqfields + [ 'machine_time', 'p_val', 'is_test' ]
 
     def __init__(self, neutrino_time=None, p_val=None, **kwargs):
         super().__init__(self.fields,
@@ -274,7 +276,7 @@ class SNEWSSignificanceTierMessage(SNEWSMessage):
     """Message for SNEWS 2.0 significance tier."""
 
     reqfields = [ 'p_values', 't_bin_width' ]
-    fields = SNEWSMessage.basefields + reqfields + [ 'machine_time' ]
+    fields = SNEWSMessage.basefields + reqfields + [ 'machine_time', 'is_test' ]
 
     def __init__(self, p_values=None, t_bin_width=None, **kwargs):
         # Type check for proper types.
@@ -306,7 +308,7 @@ class SNEWSTimingTierMessage(SNEWSMessage):
     """Message for SNEWS 2.0 timing tier."""
 
     reqfields = [ 'timing_series' ]
-    fields = SNEWSMessage.basefields + reqfields + [ 'machine_time', 'p_val' ]
+    fields = SNEWSMessage.basefields + reqfields + [ 'machine_time', 'p_val', 'is_test' ]
 
     def __init__(self, p_val=None, timing_series=None, **kwargs):
         super().__init__(self.fields,
@@ -333,7 +335,7 @@ class SNEWSRetractionMessage(SNEWSMessage):
     """Message for SNEWS 2.0 retractions."""
 
     reqfields = [ 'retract_latest' ]
-    fields = SNEWSMessage.basefields + reqfields + [ 'machine_time', 'retraction_reason' ]
+    fields = SNEWSMessage.basefields + reqfields + [ 'machine_time', 'retraction_reason', 'is_test' ]
 
     def __init__(self, retract_latest=None, retraction_reason=None, **kwargs):
         super().__init__(self.fields,
@@ -352,7 +354,7 @@ class SNEWSHeartbeatMessage(SNEWSMessage):
     """Message for SNEWS 2.0 heartbeats."""
 
     reqfields = [ 'detector_status' ]
-    fields = SNEWSMessage.basefields + reqfields + [ 'machine_time' ]
+    fields = SNEWSMessage.basefields + reqfields + [ 'machine_time', 'is_test' ]
 
     def __init__(self, machine_time=None, detector_status=None, **kwargs):
         super().__init__(self.fields,
@@ -481,12 +483,16 @@ class SNEWSMessageBuilder:
         messages_to_send = []
 
         # append meta fields if the meta field is not in the other messages
+        meta_dict = {}
         for m in self.messages:
             mes = m.message_data
             met = m.meta
             for k, v in met.items():
+                # store meta fields to append later
+                # if the meta field is not in the other messages
                 if k not in fields_set:
-                    mes[k] = v
+                    meta_dict[k] = v
+            mes["meta"] = meta_dict
             messages_to_send.append(mes)
 
         with Publisher(env_path=env_file,
