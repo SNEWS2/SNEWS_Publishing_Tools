0cf59002c1e02b91aa087c965761e668604bd26c
Cecil Gregg <cg4express@gmail.com>
2025-04-15 11:59:47 -0400

    update firedrill subscriber actions

--- snews_pt/auxiliary/firedrill_plugin.py
+++ snews_pt/auxiliary/firedrill_plugin.py
@@ -5,7 +5,6 @@ import logging
 # Configure logging
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[logging.StreamHandler(sys.stdout)],
 )
 
